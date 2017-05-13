51b6c6135f20c1733032d5eef80fd66129a464d7
Mitch Ryan <mitch.ryan@outlook.co.nz>
2017-05-14 09:20:14 +1200

    Throttle mousewatcher

--- resources/js/components/MouseWatcher.js
+++ resources/js/components/MouseWatcher.js
@@ -3,15 +3,24 @@ import { updateMousePosition } from 'state/actions';
 import React, { Component } from 'react';
 
 class MouseWatcher extends Component {
+
+    _last = Date.now();
+
     constructor(props) {
         super(props);
     }
 
     _update(e) {
-        this.props.updateMousePosition({
-            x: e.clientX,
-            y: e.clientY,
-        });
+        let now = Date.now();
+        let elapsed = Date.now() - this._last;
+
+        if (elapsed > 16) {
+            this.props.updateMousePosition({
+                x: e.clientX,
+                y: e.clientY,
+            });
+            this._last = now;
+        }
     }
 
     componentDidMount() {
